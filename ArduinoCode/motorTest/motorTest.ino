7db4119c2b47f0514570b08acb29750afcb9d542
heymosbrother <danielshencool@gmail.com>
2023-05-29 17:43:31 +0800

    correct errors

--- ArduinoCode/motorTest/motorTest.ino
+++ ArduinoCode/motorTest/motorTest.ino
@@ -7,9 +7,13 @@
 #include <std_msgs/Float32.h>
 #include "Motor.h"
 
+void callBack(const std_msgs::Float32& assignedSpeed_msg);
+
 ros::NodeHandle nh; // Declare the nodeHandler
-ros::Publisher motorSpeedReport("motorSpeedReport", &currentSpeed_msg);
-ros::Subscriber<std_msgs::Float32> sub("motorSpeedAssign", &callBack);
+// Define a float message (current mototr velocity) to publish
+std_msgs::Float32 currentSpeed_msg;
+ros::Publisher GetMotorVelocity("GetMotorVelocity_topic", &currentSpeed_msg);
+ros::Subscriber<std_msgs::Float32> SetMotorVelocity("SetMotorVelocity_topic", &callBack);
 
 Motor testMotor(10, 9, 8, 2, 11); // PWM, IN1, IN2, encoderA, encoderB
 
@@ -22,20 +26,16 @@ void callBack(const std_msgs::Float32& assignedSpeed_msg)
 	// do somthing, use motor class later
 }
 
-
-// Define a float message (current mototr velocity) to publish
-std_msgs::Float32 currentSpeed_msg;
-
 void setup()
 {
 	nh.initNode(); // Initialize the ROS node
-	nh.advertise(motorSpeedReport); // Advertise the publisher
-	nh.subscribe(motorSpeedAssign); // Subscribe to the subscriber
+	nh.advertise(GetMotorVelocity); // Advertise the publisher
+	nh.subscribe(SetMotorVelocity); // Subscribe to the subscriber
 
 	currentSpeed_msg.data = 0.0;
 
 	// Interrupt function for encoder control
-	attachInterupt(digitalPinToInterrupt(testMotor.ENC_A), readEncoder, RISING);
+	attachInterrupt(digitalPinToInterrupt(testMotor.ENC_A), readEncoder, RISING);
 }
 
 void loop()
@@ -43,7 +43,7 @@ void loop()
 	testMotor.SetVelocity(motorSpeed);
 
 	currentSpeed_msg.data = testMotor.ShowVelocity();
-	pub.publish(&currentSpeed_msg);	
+	GetMotorVelocity.publish(&currentSpeed_msg);	
 	nh.spinOnce();
 }
 
